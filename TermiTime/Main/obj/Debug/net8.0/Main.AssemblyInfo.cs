55f9d0bbfc2e270872acbb6347666a5c15abe46a
Kade Matthews <131820783+Kmxtthews@users.noreply.github.com>
2025-11-05 06:48:31 +0000

    ascii stuff added
    added ascii stuff

--- TermiTime/Main/obj/Debug/net8.0/Main.AssemblyInfo.cs
+++ TermiTime/Main/obj/Debug/net8.0/Main.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c95c5abfaa044d3510b9606419b268412a73151")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dc65bd418ba3044bbbbbcdb619f5796b408063f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
